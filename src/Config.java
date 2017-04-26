92f39447c2e40fa8cc04ebd1788eb30dc51a3817
XiaoBang(搜索事业部_企业搜索和基础平台部) <xiaobang213452@sogou-inc.com>
2017-04-26 19:09:38 +0800

    Initial commit

---
+++ src/Config.java
@@ -0,0 +1,7 @@
+/**
+ * Created by xiaobang213452 on 2017/4/26.
+ */
+public class Config {
+    public static int conns;
+    public static int
+}
---
+++ src/TestMySQL.java
@@ -0,0 +1,15 @@
+/**
+ * Created by xiaobang213452 on 2017/4/26.
+ */
+public class TestMySQL {
+
+    public static void main(String args[]){
+
+    }
+
+    public TestMySQL(){
+
+    }
+
+
+}
